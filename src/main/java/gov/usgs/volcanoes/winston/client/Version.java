d40f4a7a5fe5966f75f7cb66a88bfc6315ee30d6
tparker <tparker@usgs.gov>
2017-07-26 12:51:25 -0800

    init

---
+++ src/main/java/gov/usgs/volcanoes/winston/client/Version.java
@@ -0,0 +1,10 @@
+package gov.usgs.volcanoes.winston;
+
+public final class Version {
+
+    public static final String BUILD_TIME="2017-07-26T20:50:22Z";
+
+    public static final String POM_VERSION="1.3.0-SNAPSHOT";
+
+	public static final String VERSION_STRING = "Version: " + POM_VERSION + " Built: " + BUILD_TIME;
+}

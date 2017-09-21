5e02257a8d4c437fbe596dbd97cfd1d11b22a825
Tom Parker <tparker@usgs.gov>
2017-09-21 14:54:20 -0800

    Release ByteBuf

--- src/main/java/gov/usgs/volcanoes/wwsclient/handler/GetScnlHeliRawHandler.java
+++ src/main/java/gov/usgs/volcanoes/wwsclient/handler/GetScnlHeliRawHandler.java
@@ -66,6 +66,7 @@ public class GetScnlHeliRawHandler extends AbstractCommandHandler {
         heliData.fromBinary(ByteBuffer.wrap(bytes));
       }
       sem.release();
+      msgBuf.release();
     } else {
       LOGGER.debug("Received {} of {} bytes.", buf.size(), length);
     }
